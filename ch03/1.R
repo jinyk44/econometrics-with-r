6a56a70e313338f7712887d7e37a59dad69965c8
jinyk44 <jinyukunjarvis@gmail.com>
2021-03-15 10:19:07 +0800

    update exercise_1

---
+++ ch03/1.R
@@ -0,0 +1,16 @@
+# define a function for the estimator
+Y_tilde <- function(c){
+	sum(c)/(length(c)-1)
+}         
+         
+# repeatedly compute estimates and store the results in est_biased
+set.seed(123)
+est_biased <- replicate(Y_tilde(rnorm(5, mean = 10, sd = 25)),5)
+         
+         
+# plot a histogram of est_biased
+hist(est_biased)
+         
+         
+# add a red vertical line at mu = 10
+abline(v = 10,col = 'red')
