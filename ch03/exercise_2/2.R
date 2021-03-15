0e04ff2cc5d0b2375c12caf00f7126e8831e0952
jinyk44 <jinyukunjarvis@gmail.com>
2021-03-15 10:38:20 +0800

    update exercise_2

---
+++ ch03/exercise_2/2.R
@@ -0,0 +1,12 @@
+Y_tilde <- function(c){
+	sum(c)/(length(c)-1)
+}
+
+# compute repeatedly estimates and store the results in est_consistent
+set.seed(123)
+est_consistent <- replicate(expr = Y_tilde(rnorm(rnorm(mean = 10, sd = 25, 1000))), n = 10000)
+# plot a histogram of est_biased
+hist(est_consistent)
+
+# add a red vertical line at mu = 10
+abline(v = 10, col = 'red')
